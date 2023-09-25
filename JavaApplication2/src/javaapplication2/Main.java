47b2223a994f69c33e3626a9a09602fa384e8c34
Thilina-Dhananjaya <120640837+Thilina-Dhananjaya@users.noreply.github.com>
2023-09-25 16:09:00 +0530

    Buttons
    Set buttons

---
+++ JavaApplication2/src/javaapplication2/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author UserA
+ */
+public class Main {
+    
+    public static void main(String [] args) {
+        
+        basic lgf = new basic();
+        lgf.setVisible(true);
+        lgf.pack();
+        lgf.setLocationRelativeTo(null);
+    }
+    
+}
